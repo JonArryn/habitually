429ffcd5a674e1a2b37a1f546aa458a0818265ed
JonArryn <dumpster@lordofthevale.com>
2023-01-18 09:55:12 -0500

    WIP implementing habit context

--- frontend/src/hook/useHabit.ts
+++ frontend/src/hook/useHabit.ts
@@ -1,5 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react';
 import habits, { IHabit } from '../constant/HABITS';
+import { useParams } from 'react-router-dom';
 
 export interface IUseHabit {
   getAllHabits: () => IHabit[] | [];
@@ -12,18 +13,27 @@ const useHabit = (): IUseHabit => {
   const [allHabits, setAllHabits] = useState<IHabit[] | []>([]);
   const [currentHabit, setCurrentHabit] = useState<IHabit | undefined>();
 
+  const habitIdRef = useRef(useParams().habitId);
+
   useEffect(() => {
     setAllHabits(() => habits);
   }, []);
 
   const getAllHabits = () => allHabits;
 
-  const manageHabit = (habitId: string) => {
-    allHabits.forEach((habit) => {
-      habit.id === habitId && setCurrentHabit(habit);
-    });
-  };
+  const manageHabit = useCallback(
+    (habitId: string | undefined) => {
+      allHabits.forEach((habit) => {
+        habit.id === habitId && setCurrentHabit(habit);
+      });
+    },
+    [allHabits]
+  );
 
+  useEffect(() => {
+    console.log('set current');
+    manageHabit(habitIdRef.current);
+  }, [manageHabit]);
   const getCurrentHabit = () => currentHabit;
 
   const createNewHabit = (habit: IHabit) => {
